5e50101f3df25297f48ef404330ab3acc1afc3e4
Aaron Rodriguez <aaroner@yahoo.com>
2019-09-08 20:24:54 -0500

    14/21

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useRef, createContext } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  createContext,
+  useMemo,
+} from 'react';
 import Toggle from './Toggle';
 import Counter from './Counter';
 import { useTitleInput } from './hooks/useTitleInput';
@@ -9,6 +15,30 @@ const App = () => {
   // const [value, setValue] = useState(initialState);
   const [name, setName] = useTitleInput('');
   const ref = useRef();
+  const [dishes, setDishes] = useState([]);
+
+  const fetchDishes = async () => {
+    const res = await fetch(
+      'https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes',
+    );
+    const data = await res.json();
+    setDishes(data);
+  };
+
+  useEffect(() => {
+    fetchDishes();
+  }, []);
+
+  // const reverseWord = word => {
+  //   console.log('function called');
+  //   return word
+  //     .split('')
+  //     .reverse()
+  //     .join('');
+  // };
+  // const title = 'Level Up Dishes';
+
+  // const TitleReversed = useMemo(() => reverseWord(name), [name]);
 
   return (
     <UserContext.Provider
@@ -18,6 +48,7 @@ const App = () => {
     >
       <div className="main-wrapper" ref={ref}>
         <h1 onClick={() => ref.current.classList.add('new-fake-class')}>
+          {/* {TitleReversed} */}
           Level Up Dishes
         </h1>
         <Toggle />
@@ -35,6 +66,17 @@ const App = () => {
           />
           <button>Submit</button>
         </form>
+        {dishes.map(dish => (
+          <article className="dish-card dish-card--withImage">
+            <h3>{dish.name}</h3>
+            <p>{dish.desc}</p>
+            <div className="ingredients">
+              {dish.ingredients.map(ingredient => (
+                <span>{ingredient}</span>
+              ))}
+            </div>
+          </article>
+        ))}
       </div>
     </UserContext.Provider>
   );
